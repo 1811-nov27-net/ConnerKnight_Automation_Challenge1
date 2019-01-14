cacc111f055a24d976103c4bab0c552afb5bf710
conner knight <conneraknight@gmail.com>
2019-01-14 11:58:53 -0600

    all files

---
+++ AssessForm/AssessForm/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AssessForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssessForm.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
